bd32f1eb2f16ca57e8ebb29273319db84b64c6ae
Marco Enrique <fuzzyslash@hotmail.com>
2026-04-02 14:36:48 -0600

    Question styling scaffold

--- src/pages/Quiz/components/Question.jsx
+++ src/pages/Quiz/components/Question.jsx
@@ -1,19 +1,31 @@
+import styles from "./Question.module.css";
+
 export default function Question() {
   return (
-    <div>
-      <h3>Question</h3>
-      <ul>
-        <li>
-          <button>Answer</button>
+    <div className={styles.question}>
+      <h3 className={styles.title}>Question</h3>
+      <ul className={`${styles.answers} reset-list`}>
+        <li className={styles["answer-item"]}>
+          <button className={`${styles["answer"]} ${styles["answer--active"]}`}>
+            Answer
+          </button>
         </li>
-        <li>
-          <button>Answer</button>
+        <li className={styles["answer-item"]}>
+          <button
+            className={`${styles["answer"]} ${styles["answer--correct"]}`}
+          >
+            Answer
+          </button>
         </li>
-        <li>
-          <button>Answer</button>
+        <li className={styles["answer-item"]}>
+          <button
+            className={`${styles["answer"]} ${styles["answer--incorrect"]}`}
+          >
+            Answer
+          </button>
         </li>
-        <li>
-          <button>Answer</button>
+        <li className={styles["answer-item"]}>
+          <button className={styles["answer"]}>Answer</button>
         </li>
       </ul>
     </div>
