cea1663ae5cef48a717a2b4e5bb4150c6c911383
Marco Enrique <fuzzyslash@hotmail.com>
2026-04-02 14:01:41 -0600

    Intro base styles

--- src/pages/Home/components/Intro/Intro.jsx
+++ src/pages/Home/components/Intro/Intro.jsx
@@ -1,9 +1,13 @@
+import styles from "./Intro.module.css";
+
 export default function Intro() {
   return (
-    <div>
-      <h1>Quizzical</h1>
-      <p>Test your knowledge with quick, fun quizzes.</p>
-      <button>Start Quiz</button>
+    <div className={styles.intro}>
+      <h1 className={styles.title}>Quizzical</h1>
+      <p className={styles.description}>
+        Test your knowledge with quick, fun quizzes.
+      </p>
+      <button className={styles.button}>Start Quiz</button>
     </div>
   );
 }
